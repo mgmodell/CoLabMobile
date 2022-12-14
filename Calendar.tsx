b688beb7712133b93a55332a2762cac35dfdf672
DennisR2828 <112634917+DennisR2828@users.noreply.github.com>
2022-12-14 08:24:26 -0500

    Create Calendar.tsx
    I hope this works. This is the Calendar component

---
+++ Calendar.tsx
@@ -0,0 +1,120 @@
+import React, {useState, useMemo, useCallback} from 'react';
+import {StyleSheet, Text, View, TextStyle} from 'react-native';
+import {CalendarList, DateData} from 'react-native-calendars';
+
+
+const RANGE = 24;
+const initialDate = '2022-12-09';
+const nextWeekDate = '2022-12-16';
+const nextMonthDate = '2023-01-05';
+
+interface Props {
+  horizontalView?: boolean;
+}
+
+const CalendarListScreen = (props: Props) => {
+  const {horizontalView} = props;
+  const [selected, setSelected] = useState(initialDate);
+  const marked = useMemo(() => {
+    return {
+      [nextWeekDate]: {
+        selected: selected === nextWeekDate,
+        selectedTextColor: '#5E60CE',
+        marked: true
+      },
+      [nextMonthDate]: {
+        selected: selected === nextMonthDate,
+        selectedTextColor: '#5E60CE',
+        marked: false
+      },
+      [selected]: {
+        selected: true,
+        disableTouchEvent: true,
+        selectedColor: '#5E60CE',
+        selectedTextColor: 'white'
+      }
+    };
+  }, [selected]);
+
+  const onDayPress = useCallback((day: DateData) => {
+    setSelected(day.dateString);
+  }, []);
+
+  return (
+    <CalendarList
+      
+      current={initialDate}
+      pastScrollRange={RANGE}
+      futureScrollRange={RANGE}
+      onDayPress={onDayPress}
+      markedDates={marked}
+      renderHeader={!horizontalView ? renderCustomHeader : undefined}
+      calendarHeight={!horizontalView ? 390 : undefined}
+      theme={!horizontalView ? theme : undefined}
+      horizontal={horizontalView}
+      pagingEnabled={horizontalView}
+      staticHeader={horizontalView}
+    />
+  );
+};
+
+const theme = {
+  stylesheet: {
+    calendar: {
+      header: {
+        dayHeader: {
+          fontWeight: '600',
+          color: '#48BFE3'
+        }
+      }
+    }
+  },
+  'stylesheet.day.basic': {
+    today: {
+      borderColor: '#48BFE3',
+      borderWidth: 0.8
+    },
+    todayText: {
+      color: '#5390D9',
+      fontWeight: '800'
+    }
+  }
+};
+
+function renderCustomHeader(date: any) {
+  const header = date.toString('MMMM yyyy');
+  const [month, year] = header.split(' ');
+  const textStyle: TextStyle = {
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingTop: 10,
+    paddingBottom: 10,
+    color: '#5E60CE',
+    paddingRight: 5
+  };
+
+  return (
+    <View style={styles.header}>
+      <Text style={[styles.month, textStyle]}>{`${month}`}</Text>
+      <Text style={[styles.year, textStyle]}>{year}</Text>
+    </View>
+  );
+}
+
+export default CalendarListScreen;
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    marginBottom: 10
+  },
+  month: {
+    marginLeft: 5
+  },
+  year: {
+    marginRight: 5
+  }
+});
